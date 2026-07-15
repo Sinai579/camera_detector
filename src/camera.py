36f9a8bb7e75e148969edb3538553320dbb87ddc
Sinai579 <al22760579@ite.edu.mx>
2026-07-15 10:22:09 -0700

    Add initial project structure with configuration and requirements files

---
+++ src/camera.py
@@ -0,0 +1,39 @@
+"""
+=========================================================
+File: camera.py
+
+Project:
+    Camera Detector
+
+Description:
+    Handles all camera operations including initialization,
+    frame acquisition, image capture, and resource management.
+
+Author:
+    Sinai Cabrera Retana
+
+Created:
+    July 2026
+=========================================================
+"""
+
+class Camera:
+    #Camera management class that encapsulates all operations related to camera handling.
+    
+    def __init__(self): #Camera attributes
+        pass
+    def open(self):
+        pass
+    def close(self):
+        pass
+    def read_frame(self):
+        pass
+    def show_frame(self, frame):
+        pass
+    def capture_image(self):
+        pass
+    def change_resolution(self, width, height):
+        pass
+    def is_open(self):
+        pass
+    
---
+++ src/config.py
@@ -0,0 +1,10 @@
+#General config of the camera
+
+CAMERA_INDEX = 0
+
+FRAME_WIDTH = 0
+FRAME_HEIGHT = 0
+
+WINDOW_NAME = "CAMERA_DET"
+IMAGE_FORMAT = ".jpg"
+CAPURE_FOLDER = "../dataset"
